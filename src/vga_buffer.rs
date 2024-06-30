2c2237ac479998976e44653c03525dc9f484c437
lawrenzo-pro <57544775+lawrenzo-pro@users.noreply.github.com>
2024-06-30 12:30:39 +0300

    Update vga_buffer.rs

--- src/vga_buffer.rs
+++ src/vga_buffer.rs
@@ -112,7 +112,7 @@ lazy_static! {
     pub static ref WRITER :Mutex<Writer>= Mutex::new(Writer{
         column_position: 0,
         color_code: ColorCode::new(Color::Yellow, Color::Black),
-        buffer: unsafe { &mut *(0xb8000 as *mut Buffer) },
+        buffer: unsafe { &mut *(0xb8000 as *mut Buffer) },//Deref the raw pointer, the vga buffer starts at 0xb8000 on IBM compatibles 
     });
 }
 
